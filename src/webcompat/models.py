257aa7db28bb222d4f808b5cd294c5103d165589
Ksenia Berezina <kberezina@mozilla.com>
2026-02-04 16:48:55 -0500

    Incoming reports similarity clustering

--- src/webcompat/models.py
+++ src/webcompat/models.py
@@ -66,15 +66,12 @@ class Report:
 
     def create_signature(self) -> Signature:
         """Create a default signature"""
-        return Signature(
-            json.dumps(
-                {
-                    "symptoms": [
-                        {"type": "url", "part": "hostname", "value": self.url.hostname}
-                    ]
-                }
-            )
-        )
+        symptoms = [{"type": "url", "part": "hostname", "value": self.url.hostname}]
+
+        if self.cluster_id is not None:
+            symptoms.append({"type": "cluster_id", "value": self.cluster_id})
+
+        return Signature(json.dumps({"symptoms": symptoms}))
 
 
 @dataclass
